cb26d729b8a7b8e240d6d24e39bf6d0e8baadf8b
johnathan <johnathanfercher22@gmail.com>
2018-06-02 19:38:33 -0300

    removido lixos

--- cmake-build-debug/CMakeFiles/vss-joystick-core.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libvss-joystick-core.a"
-)
--- cmake-build-debug/third-party/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/johnathan/Repositories/VSS-SDK/VSS-Joystick/third-party/googletest/googletest
-# Build directory: /home/johnathan/Repositories/VSS-SDK/VSS-Joystick/cmake-build-debug/third-party/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
